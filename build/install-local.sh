e1cd98a0bc214ea646707b1ed83be51a9ee5cb6d
jilen <jilen.zhang@gmail.com>
2022-06-20 10:21:31 +0800

    fix

--- build/install-local.sh
+++ build/install-local.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/com-lihaoyi/mill.git /tmp/mill
 cd /tmp/mill
-sed -i 's/Info/Debug/g' ./scalalib/worker/src/mill/scalalib/worker/ZincWorkerImpl.scala
+sed -i 's/sbt\.util\.Level\.Info/sbt.util.Level.Debug/g' ./scalalib/worker/src/mill/scalalib/worker/ZincWorkerImpl.scala
 ./mill installLocal
